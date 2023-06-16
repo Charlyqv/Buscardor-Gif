f129721c1a8ef7400e79a9551991bfe0181518ac
Carlos <charlyqv98@gmail.com>
2023-06-16 08:32:20 -0600

    Pruebas del componente - GifGridItem

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 
-export const GifItem = ({ tittle, url, id }) => {
+export const GifItem = ({ title, url, id }) => {
 
   return (
     <div className="card">
-      <img src={ url } alt={ tittle } />
-      <p>{ tittle }</p>
+      <img src={ url } alt={ title } />
+      <p>{ title }</p>
     </div>
   )
 }
 
 GifItem.propTypes = {
-  tittle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   url   : PropTypes.string.isRequired,
 }
