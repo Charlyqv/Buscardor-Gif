e08e72ae35ebd8cf3b654c84650ec6ec283b7509
Carlos <charlyqv98@gmail.com>
2023-06-20 09:15:40 -0600

    Hacer un mock completo de un Custom Hook

--- src/tests/components/GifGrid.test.jsx
+++ src/tests/components/GifGrid.test.jsx
@@ -1,13 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
 
-
+jest.mock('../../hooks/useFetchGifs');
 
 describe('Prueba en <GifGrid />', () => {
 
   const category = 'Pokémon';
 
   test('debe de mostrar el loading inicialmente', () => {
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
 
     render( <GifGrid category={ category } />);
     expect( screen.getByText('Cargando...') );
@@ -16,6 +22,28 @@ describe('Prueba en <GifGrid />', () => {
   });
 
   test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => {
+
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Pikachu',
+        url: 'https://holiholiholi'
+      },
+      {
+        id: '123',
+        title: 'Charizard',
+        url: 'https://charizard'
+      },
+    ]
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render( <GifGrid category={ category } />);
+    
+    expect( screen.getAllByRole('img').length ).toBe(2);
 
   });
 
