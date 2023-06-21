31a0487a8a0727471ef6648ce4aa346d5204a53c
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-06-20 22:03:17 -0600

    Pruebas de tarea

---
+++ src/tests/GifExpertApp.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { GifExpertApp } from '../GifExpertApp';
+
+describe('Pruebas en el GifExpertApp', () => { 
+  
+  test('should', () => { 
+
+    render( <GifExpertApp/> );
+    screen.debug();
+
+   });
+ });
